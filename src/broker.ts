3877287ac3cb08b62f7de4dec4a0d4ed3cabdfa0
Felipe Mendes Braga <felipe.mendes@urpay.com.br>
2019-11-06 12:01:26 -0300

    Funçoes comentadas

--- src/broker.ts
+++ src/broker.ts
@@ -1,7 +1,6 @@
 import createLogger from "logging";
 import * as uuidv4 from "uuid/v4";
 import { connect, Options, Channel, ConsumeMessage } from "amqplib";
-import { optionalCallExpression } from "@babel/types";
 
 export const logger = createLogger("broker");
 
@@ -156,9 +155,13 @@ export class Broker {
     this._config = Object.assign({}, defaultConfig, _config);
     this._exchanges = this._config.exchanges;
     this._queues = this._config.queues;
-    this.connect = this.connect.bind(this);
   }
 
+  /**
+   * Set a sleep time
+   * 
+   * @param ms miliseconds
+   */
   public sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
@@ -178,29 +181,30 @@ export class Broker {
   }
 
   /**
-   * Conexão
+   * Connection
    */
   public get conn() {
     return this._connection;
   }
 
   /**
-   * Canal
+   * Channel
    */
   public get channel() {
     return this._channel;
   }
 
   /**
-   * Conecta com o rabbitmq
+   * Connect with RabbitMQ
    */
-  private async connect() {
-    // Verifica se já está conectado
+  private connect = async () => {
+    // Verify if is already connected
     if (this._connection !== null) {
       logger.info("[connect] your broker is already connected.");
       return;
     }
 
+    // Connection options
     const port: number = this._config.connection.port
       ? parseInt(this._config.connection.port)
       : 5672;
@@ -212,12 +216,16 @@ export class Broker {
       username: this._config.connection.user,
       password: this._config.connection.pass
     };
+
+    // Verify if have a SSL certificate
     const options: any =
       this._config.connection.protocol == "amqps"
         ? {
             ca: [this._config.connection.certificate]
           }
         : {};
+
+    // Try connect
     try {
       logger.info(
         `[connect] going to connect to ${this._config.connection.host}:${this._config.connection.port}`
@@ -227,18 +235,23 @@ export class Broker {
         url,
         options
       );
+      // Save the channel
       this._channel = await this._connection.createChannel();
 
+      // Connection events
       this._connection.on("error", (err: any) => {
         this._connection = null;
         if (err.message !== "connection closing") {
           logger.error("Conn error: ", err.message);
         }
       });
+
       this._connection.on("close", () => {
         this._connection = null;
         logger.error("Connection closed!");
         logger.error("reconnecting ..");
+
+        // Try to reconnect
         setTimeout(this.connect, 1000);
       });
       logger.info(
@@ -248,12 +261,14 @@ export class Broker {
       logger.error("Error trying to connection to " + url);
       logger.error(e.message);
       this._connection = null;
+      
+      // Try to reconnect
       setTimeout(this.connect, 1000);
     }
   }
 
   /**
-   * Fecha a conexão
+   * Close the channel connection connection
    */
   public async close() {
     if (this._channel) {
@@ -263,8 +278,15 @@ export class Broker {
     logger.info("Connection is closed!");
   }
 
+  /**
+   * Initialize the Broker service
+   */
   public async init() {
+
+    // Connect to RabbitMQ
     await this.connect();
+
+    // Assert all exchanges
     try {
       await Promise.all(
         this._exchanges.map((ex: any) => {
@@ -279,6 +301,7 @@ export class Broker {
       logger.info(e);
     }
 
+    // assert and bind all queues
     for (let v of this._queues) {
       await this.createQueue(v);
     }
@@ -287,14 +310,26 @@ export class Broker {
     return
   }
 
+  /**
+   * Add the consumer into the broker
+   * 
+   * @param queue queue name
+   * @param cb Consumer function
+   */
   public addConsume(queue: string, cb: (msg: ConsumeMessage) => Promise<Object>) {
+
     this._consumes.set(queue, async (msg: ConsumeMessage) => {
       logger.info("Running consumer " + queue);
+      // Call the consumer function
       let response = await cb(msg);
+
+      // Verify if exists a replyTo queue to send back
       if (this._channel) {
         this._channel.ack(msg);
         if (msg.properties.replyTo) {
           logger.info("Replying to " + msg.properties.replyTo);
+
+          // Send back to broker sender
           this._channel.sendToQueue(
             msg.properties.replyTo,
             Buffer.from(JSON.stringify(response))
@@ -304,6 +339,11 @@ export class Broker {
     });
   }
 
+  /**
+   * Assert a queue and bind to a exchange
+   * 
+   * @param q Queue options
+   */
   private createQueue = async (q: Queue) => {
     if (!this._channel) {
       throw new Error("channel not initialized");
@@ -329,6 +369,12 @@ export class Broker {
     return typeof msg === "object" ? JSON.stringify(msg) : msg;
   }
 
+  /**
+   * Create a consumer to receive response from a worker
+   * 
+   * @param replyTo queue to reply
+   * @param exchange Exchange to queue
+   */
   private async consumeResponse(
     replyTo: string,
     exchange? : string
@@ -336,26 +382,35 @@ export class Broker {
     if (!this._channel) {
       throw new Error("channel not initialized");
     }
-    //registra a fila no servidor caso não exista
+    // create a queue with replyTo name
     const q = await this._channel.assertQueue(replyTo, {
       durable: false,
       autoDelete: true
     });
 
-    //faz a ligação com a fila
+    // bind the queue with exchange
     if(exchange) {
       await this._channel.bindQueue(q.queue, exchange, replyTo);
     }
 
+    // wait for response
     const response = await this._channel.get(q.queue);
+
+    // If is false, try again
     if (!response) {
       await this.sleep(100);
       return this.consumeResponse(replyTo, exchange);
     }
 
+    // return the string value
     return response.content.toString();
   }
 
+  /**
+   * publish a message to a exchange key pattern
+   * 
+   * @param publishOptions options to publish
+   */
   public async publishMessage(publishOptions: PublishOptions) {
     let response;
     let replyTo = ""
@@ -366,19 +421,29 @@ export class Broker {
     console.log("publishing...");
     console.log(publishOptions.msg);
     
+    // publish the message
     this.publish(
       publishOptions.exchange,
       publishOptions.key,
       publishOptions.msg,
       publishOptions.options
     );
+
+    // if rpc is setted, wait from the response to return
     if(publishOptions.rpc) {
       response = this.consumeResponse(replyTo, publishOptions.exchange);
     }
     
-    return response;
+    return await response;
   }
 
+  /**
+   * Publish a message
+   * @param exchange Exchange name
+   * @param key key
+   * @param msg Data to send
+   * @param options Exchange publish options
+   */
   private publish(
     exchange: string,
     key: string,
@@ -399,9 +464,15 @@ export class Broker {
     );
   }
 
+  /**
+   * Send a message to a especific queue
+   * 
+   * @param sendOptions Options to send
+   */
   public async sendMessage(sendOptions : SendToQueueOptions) {
     let response;
 
+    // if rpc is setted, wait from the response to return
     if (sendOptions.rpc) {
       const replyTo = uuidv4();
       response = this.consumeResponse(replyTo);
@@ -415,6 +486,9 @@ export class Broker {
     return await response;
   }
 
+  /**
+   * send a message to a queue
+   */
   private sendToQueue = (
     queue: string,
     content: string | Object,
